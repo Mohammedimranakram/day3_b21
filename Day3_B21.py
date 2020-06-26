d8ceda92f1f6d647f8b7868186586bea746f10c8
Mohammedimranakram <mohammedimranakram1@gmail.com>
2020-06-26 09:54:02 +0530

    Add files via upload

---
+++ Day3_B21.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Introduction of f string: **
+
+
+# In[1]:
+
+
+first_name = 'imran'
+
+
+# In[2]:
+
+
+last_name = 'akram'
+
+
+# In[ ]:
+
+
+#general syntax of a f string:
+#f"{place holder} {placeholder}"
+
+
+# In[3]:
+
+
+full_name = f"{first_name} {last_name}" # f string is being implemented...!
+
+
+# In[4]:
+
+
+print(full_name)
+
+
+# In[ ]:
+
+
+#enhacement of the code
+
+
+# In[5]:
+
+
+print(full_name.title())
+
+
+# In[6]:
+
+
+print(full_name.upper())
+
+
+# In[ ]:
+
+
+# further enchancement of the code...!
+
+
+# In[ ]:
+
+
+#keep up the good work, imran
+
+
+# In[7]:
+
+
+print(f"kep up the good work, {full_name}")
+
+
+# In[9]:
+
+
+print(f"keep up the good work, {full_name.title()}")
+
+
+# In[ ]:
+
+
+#Adding white spaces to strings:
+
+
+# In[10]:
+
+
+print('python')
+
+
+# In[11]:
+
+
+print('/tpython') #tab delimter
+
+
+# In[ ]:
+
+
+print()
+
+
+# In[1]:
+
+
+print('\tpython') #tab delimter
+
+
+# In[2]:
+
+
+print('fav_languages:\npython\nc++\nc\njava') #\n---> new line delimter
+
+
+# In[ ]:
+
+
+#removing white spaces:
+
+
+# In[3]:
+
+
+fav_lang = ' python'
+
+
+# In[4]:
+
+
+print(fav_lang)
+
+
+# In[5]:
+
+
+fav_lang.1strip()
+
+
+# In[6]:
+
+
+fav_lang.rstrip()
+
+
+# In[7]:
+
+
+fav_lang2 = 'java'
+
+
+# In[9]:
+
+
+fav_lang2.rstrip()
+
+
+# In[ ]:
+
+
+
+
